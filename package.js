383e920240e81c035ffd4dade89cd17f1c0f52ab
pruthvi <pruthvi.n028@gmai.com>
2026-07-01 16:54:03 +0530

    testing node app

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "matrix-test-demo",
+  "version": "1.0.0",
+  "description": "Minimal project for GitHub Actions matrix testing",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "node --test"
+  },
+  "dependencies": {},
+  "devDependencies": {}
+}
